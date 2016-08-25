8d001a0f6982f076bd821159582ddc69aee9e4db
chenlianghong <clh021@gmail.com>
2016-08-25 12:56:32 +0800

    Create installApps.sh

---
+++ installApps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Install some apps
+sudo apt install -y filezilla#ftp
+sudo apt install -y virtualbox virtualbox-qt virtualbox-ext-pack#虚拟机
+sudo apt install -y remmina remmina-plugin-rdp remmina-plugin-vnc#远程链接
+sudo apt install -y workrave#定时休息软件
+sudo apt install -y backintime-common backintime-gnome#系统还原点
