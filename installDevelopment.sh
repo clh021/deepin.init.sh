6b42375c16b86ae6b06ab8010c193dfcd7d65ef9
chenlianghong <clh021@gmail.com>
2016-08-30 11:34:53 +0800

    update apps

--- installDevelopment.sh
+++ installDevelopment.sh
@@ -2,7 +2,9 @@
 sudo apt install -y curl
 sudo apt install -y git
 sudo apt install -y vim
-sudo apt install -y sublime-text
+sudo apt install -y sublime-text-dev #sublime3体验更好
+sudo apt install -y filezilla #ftp
+sudo apt install -y remmina remmina-plugin-rdp remmina-plugin-vnc #远程链接
 #sudo apt-get autoremove
 
 #以下如果您有自己的操作，请自行修改
