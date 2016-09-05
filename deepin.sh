2992ef5ac5b8e30b6cc1639f65fca06890331298
chenlianghong <clh021@gmail.com>
2016-09-05 10:36:33 +0800

    add golang and update apps google https

--- deepin.sh
+++ deepin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt update
-apt upgrade
+# apt update
+# apt upgrade
 
 # Simple setup for configuring Ubuntu quickly.
 sudo chmod +x *.sh 
@@ -8,6 +8,16 @@ sudo ./installApps.sh
 sudo ./installDevelopment.sh
 sudo ./setHost.sh
 sudo ./setBackground.sh
+sudo ./removeApps.sh
 #sudo apt-get autoremove
 
-#卸载深度的某些软件
+#提示一些别的操作
+echo "强制谷歌访问使用https
+谷歌浏览器，打开chrome://net-internals/#hsts
+在'Add domain'中依次输入以下网址并勾下面俩勾,Add
+google.com
+google.com.hk
+googleusercontent.com
+googleapis.com
+
+搜狗输入法候选字符9个字符"
