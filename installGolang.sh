97eeeffe93f5701775cca7efcb5fe140ebba5208
chenlianghong <clh021@gmail.com>
2016-09-05 11:06:10 +0800

    update for install golang

--- installGolang.sh
+++ installGolang.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #wget -c https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.7.linux-amd64.tar.gz #安装Golang基本程序
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile #设置GO主体环境变量
-echo 'export GOROOT=$HOME/go' >> ~/.profile #设置GO程序包根目录
-echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.profile #设置GO程序包环境变量目录
 echo 'export GOPATH=/app/gopath' >> ~/.profile #设置GO程序包环境变量目录
-
-echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.profile #为了方便
+echo 'export GOROOT=/usr/local/go' >> ~/.profile #设置GO主体程序包根目录
+echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> ~/.profile #设置GO主体环境变量
 source ~/.profile #使环境变量生效
 
 go get github.com/astaxie/beego
