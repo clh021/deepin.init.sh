9ea6e1d953150a9296ea98e2b6088917be3b7168
chenlianghong <clh021@gmail.com>
2016-09-20 12:38:01 +0800

    Update tips.sh

--- tips.sh
+++ tips.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #tips
 #提示一些别的操作
-echo "您可能需要手动完成的其它操作：
+echo "\033[31m您可能需要手动完成的其它操作：\033[0m
 1、强制谷歌访问使用https
   谷歌浏览器，打开 chrome://net-internals/#hsts 在'Add domain'中依次输入以下网址并勾下面俩勾：
     google.com
