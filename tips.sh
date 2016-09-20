a6206103fd3844c30019ec8ee89e188060dbf349
chenlianghong <clh021@gmail.com>
2016-09-20 12:36:08 +0800

    Update tips.sh

--- tips.sh
+++ tips.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #tips
 #提示一些别的操作
-echo "
+echo "您可能需要手动完成的其它操作：
 1、强制谷歌访问使用https
-  谷歌浏览器，打开 chrome://net-internals/#hsts 在'Add domain'中依次输入以下网址并勾下面俩勾:
+  谷歌浏览器，打开 chrome://net-internals/#hsts 在'Add domain'中依次输入以下网址并勾下面俩勾：
     google.com
     google.com.hk
     googleusercontent.com
