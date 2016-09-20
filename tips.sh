0a3b471114b94faeb06b73dbbb043ee6203e832c
chenlianghong <clh021@gmail.com>
2016-09-20 12:34:28 +0800

    Update tips.sh

--- tips.sh
+++ tips.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #tips
 #提示一些别的操作
-echo "强制谷歌访问使用https
-谷歌浏览器，打开chrome://net-internals/#hsts
-在'Add domain'中依次输入以下网址并勾下面俩勾:
-google.com
-google.com.hk
-googleusercontent.com
-googleapis.com
+echo "
+1、强制谷歌访问使用https
+  谷歌浏览器，打开 chrome://net-internals/#hsts 在'Add domain'中依次输入以下网址并勾下面俩勾:
+    google.com
+    google.com.hk
+    googleusercontent.com
+    googleapis.com
 
-搜狗输入法候选字符9个字符
+2、搜狗输入法候选字符9个字符
 
-设置workrave为开机启动"
+3、设置workrave为开机启动"
