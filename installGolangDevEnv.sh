9625bbdd886a7159a29ffb5e1ae6b3452321867d
chenlianghong <clh021@gmail.com>
2016-09-21 20:19:55 +0800

    Update installGolangDevEnv.sh

--- installGolangDevEnv.sh
+++ installGolangDevEnv.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 sudo apt install -y curl git vim ctags
+
+# 一些必备库
+# gocode is used by many editors to provide intellisense
+go get github.com/nsf/gocode
+# goimports is something you should run when saving code to fill in import paths
+go get golang.org/x/tools/cmd/goimports
+# gorename is used by many editors to provide identifier rename support
+go get golang.org/x/tools/cmd/gorename
+# oracle is a tool that help with code navigation and search
+go get golang.org/x/tools/cmd/oracle
+# golint should be run after every build to check your code
+go get github.com/golang/lint/golint
+
 go get github.com/astaxie/beego
 go get github.com/beego/bee
 # go get github.com/qiangxue/golang-restful-starter-kit
