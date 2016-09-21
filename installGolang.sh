b377beb89e52d67d339151178f128bb0b987a323
chenlianghong <clh021@gmail.com>
2016-09-21 16:44:53 +0800

    Update installGolang.sh

--- installGolang.sh
+++ installGolang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 wget -c https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.7.linux-amd64.tar.gz #安装Golang基本程序
-sudo mkdir ~/app/gopath -p
+mkdir ~/app/gopath -p
 echo 'export GOPATH=~/app/gopath' >> ~/.profile #设置GO程序包环境变量目录
 echo 'export GOROOT=/usr/local/go' >> ~/.profile #设置GO主体程序包根目录,bee new 安装的地方
 echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> ~/.profile #设置GO主体环境变量
