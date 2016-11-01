a9d91b393acbaf560e02a51586fe7789f74e33fe
chenlianghong <clh021@gmail.com>
2016-11-01 10:46:03 +0800

    Update installDocker_.sh

--- installDocker_.sh
+++ installDocker_.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-curl -sSL https://get.docker.com/ | sudo sh
+# curl -sSL https://get.docker.com/ | sudo sh
+curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
+sudo mkdir -p /etc/systemd/system/docker.service.d 
+sudo tee /etc/systemd/system/docker.service.d/mirror.conf <<-'EOF' 
+[Service] 
+ExecStart= 
+ExecStart=/usr/bin/docker daemon -H fd:// --registry-mirror=https://eko4ves3.mirror.aliyuncs.com 
+EOF 
+sudo systemctl daemon-reload 
+sudo systemctl restart docker
