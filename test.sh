19d61b1254d5d520864ea9539f12bd72d3c5717c
chenlianghong <clh021@gmail.com>
2016-11-03 13:32:50 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,6 @@
 # PRE_URL=https://raw.githubusercontent.com/clh021/sh/master/
 # curl ${PRE_URL}test.sh | sh
 # curl https://raw.githubusercontent.com/clh021/sh/master/test.sh | sh
-sudo tee ~/mirror.conf <<-'EOF' 
-[Service] 
-ExecStart= 
-ExecStart=/usr/bin/docker daemon -H fd:// --registry-mirror=https://eko4ves3.mirror.aliyuncs.com 
-EOF
-ls
-git status
+wget -c https://raw.githubusercontent.com/racaljk/hosts/master/tools/lhosts
+chmod +x lhosts
+./lhosts -h
