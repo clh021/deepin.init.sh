c101f3334909cee099003ebc517d750bb975a588
chenlianghong <clh021@gmail.com>
2017-02-04 15:19:14 +0800

    Update installDevelopment_.sh

--- installDevelopment_.sh
+++ installDevelopment_.sh
@@ -3,6 +3,7 @@ sudo apt install -y curl
 sudo apt install -y git
 #sudo apt-get autoremove
 
+git config --global push.default simple
 #以下如果您有自己的操作，请自行修改
 git config --global user.email "clh021@gmail.com"
 git config --global user.name "chenlianghong"
