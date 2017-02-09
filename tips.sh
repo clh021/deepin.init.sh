35a85bc5b06cbee7e05fdab05c1abf77c1584f94
chenlianghong <clh021@gmail.com>
2017-02-09 21:26:34 +0800

    Update tips.sh

--- tips.sh
+++ tips.sh
@@ -2,17 +2,5 @@
 #tips
 #提示一些别的操作
 echo "\033[31m您可能需要手动完成的其它操作：\033[0m
-1、强制谷歌访问使用https
-  谷歌浏览器，打开 chrome://net-internals/#hsts 在'Add domain'中依次输入以下网址并勾下面俩勾：
-    google.com
-    google.com.hk
-    googleusercontent.com
-    googleapis.com
-
-2、搜狗输入法候选字符9个字符
-
-3、设置workrave为开机启动
-
-4、使用backintime备份好系统方便以后随时恢复
-
-5、同步浏览器私人信息"
+1、设置workrave为开机启动
+"
