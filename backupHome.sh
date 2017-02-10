d982dda50fc5f1f346db11ac54bcf3119ee9aa6e
chenlianghong <clh021@gmail.com>
2017-02-10 11:21:16 +0800

    Update backupHome.sh

--- backupHome.sh
+++ backupHome.sh
@@ -4,8 +4,6 @@
 #需要跟踪备份的内容
 #壁纸
 echo 'Pictures/Wallpapers' > backupHome.list
-#浏览器的内容，扩展，收藏，历史记录
-echo '.config/google-chrome' >> backupHome.list
 #候选字数从5修改为9
 echo '.config/SogouPY/sogouEnv.ini' >> backupHome.list
 echo '.config/fcitx/config' >> backupHome.list
@@ -20,13 +18,35 @@ echo '.bashrc' >> backupHome.list
 echo '.vimrc' >> backupHome.list
 #ssh配置
 echo '.ssh' >> backupHome.list
-#绿色软件包
+
+#php软件包
+echo '.composer' >> backupHome.list
+
 #可以自己灵活控制多版本并快速移植
-echo '.programs' >> backupHome.list
+echo '.programs' > backupHome.list
 #npm软件包
 #可以自己把握软件版本，同时可快速移植
 echo '.npm' >> backupHome.list
-#php软件包
-echo '.composer' >> backupHome.list
+echo '.profile' >> backupHome.list
+#浏览器的内容，扩展，收藏，历史记录
+echo '.config/google-chrome' > backupHome.list
 tar -cvpznf homebak.`date +%Y%m%d%H%M%S`.tar.gz -T backupHome.list
+
+
+
+#浏览器的内容，扩展，收藏，历史记录
+echo '.config/google-chrome' > backupHome.list
+tar -cvpznf homebak.chrome.`date +%Y%m%d%H%M%S`.tar.gz -T backupHome.list
+
+
+
+#可以自己灵活控制多版本并快速移植
+echo '.programs/node-v6.9.5-linux-x64' > backupHome.list
+#npm软件包
+#可以自己把握软件版本，同时可快速移植
+echo '.npm' >> backupHome.list
+echo '.profile' >> backupHome.list
+tar -cvpznf homebak.npm.`date +%Y%m%d%H%M%S`.tar.gz -T backupHome.list
+
+
 rm backupHome.list
